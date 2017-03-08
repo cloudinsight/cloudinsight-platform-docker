9f4ff92987505a52e3a2d6441d7efa947d320758
saber <zhongnanmikky@163.com>
2017-03-08 19:06:05 +0800

    add kafka-producer&kafka-consumer

---
+++ kafka-producer/start-kafka-producer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# JMX settings
+if [ -z "$KAFKA_JMX_OPTS" ]; then
+  KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false "
+fi
+
+# JMX port to use
+if [  $JMX_PORT ]; then
+  KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT "
+fi
+
+# Run Producer
+exec java $KAFKA_JMX_OPTS -cp *.jar com.bin.kafkaSend.SenderTest
